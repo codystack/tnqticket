0842574a371d311da3c8baa18af6b7a6788ebb19
codystack <ebuzzadvert@gmail.com>
2020-09-05 22:57:37 +0100

    new files

--- index.php
+++ index.php
@@ -7,16 +7,11 @@ include ('./components/header.php');
       <div class="container layer-2">
         <div class="row py-6 min-vh-80 align-items-center" data-aos="fade-up">
           <div class="col-lg-9 col-xl-8 mt-7">
-            <h5>Style, Glamour & Beauty</h5>
+            <h5>Beauty for purpose.</h5>
             <h1 class="display-0">TNQ2020</h1>
             <p class="lead mb-4">Witness the Biggest Night of Beauty Live and Safe.</p>
             <a href="#ticket" class="btn btn-primary btn-lg">Purchase Tickets</a>
           </div>
-        </div>
-      </div>
-      <div class="decoration-wrapper">
-        <div class="decoration bottom right" data-jarallax-element="-50">
-          <img class="bg-white" src="assets/img/decorations/deco-dots-1.svg" alt="deco-dots-1 decoration" data-inject-svg />
         </div>
       </div>
       <div class="divider flip-x">
@@ -26,12 +21,12 @@ include ('./components/header.php');
     <section class="bg-primary-3 text-light has-divider">
       <div class="container">
         <div class="row justify-content-between">
-          <div class="col-md-6 mb-3 mb-md-0">
-            <h2 class="h1">Witness the crowning of a new queen.</h2>
+          <div class="col-md-8 mb-3 mb-md-0">
+            <h2 class="h1">Witness the crowning of a new Queen.</h2>
             <p class="lead">Show support to your favourite contestant by getting a ticket at a discounted price to be there live at the grand finale of the biggest night of beauty.</p>
-            <p class="lead">#RoadToNigerianQueen2020</p>
+            <p class="lead">#roadtonigerianqueen2020</p>
           </div>
-          <div class="col-md-5">
+          <!--<div class="col-md-5">
             <small class="text-uppercase mb-4">Sponsored By</small>
             <ul class="d-flex flex-wrap list-unstyled">
               <li class="mr-4 mb-5" data-aos="fade-up" data-aos-delay="100">
@@ -50,7 +45,7 @@ include ('./components/header.php');
                 <img class="bg-white icon icon-logo icon-md" src="assets/img/logos/brand/time.svg" alt="icon" data-inject-svg />
               </li>
             </ul>
-          </div>
+          </div>-->
         </div>
       </div>
       <div class="divider">
@@ -63,23 +58,23 @@ include ('./components/header.php');
         <div class="row mb-4 justify-content-center text-center">
           <div class="col-xl-8 col-lg-9">
             <h2 class="display-4">Tickets selling fast</h2>
-            <p class="lead">Watch and cheer your favourite from LIVE! <br>while keeping safe distance. </p>
+            <p class="lead">Watch and cheer your favourite contestant LIVE! <br>while keeping safe distance. </p>
           </div>
         </div>
         <div class="row justify-content-center">
-          <div class="col-lg-8 col-xl-7" data-aos="fade-up">
+          <div class="col-lg-8 col-xl-8" data-aos="fade-up">
             <div class="card card-lg card-body flex-sm-row align-items-center pricing-ticket bg-primary layer-2 text-light">
               <div class="mx-2 text-center text-sm-left mb-3 mb-sm-0">
                 <span class="h1 d-block">VIP Experience</span>
                 <p class="lead pr-xl-3">
-                  Every workshop, talk and event, all weekend.
+                  Beauty, Glamour, Elegance.
                 </p>
                 <a href="#" class="btn btn-lg mt-2 btn-white">Buy Ticket Now</a>
               </div>
               <div class="mx-2 text-center">
                 <div class="d-flex align-items-center">
                   <span class="h3 mb-0">₦</span>
-                  <span class="display-3">35K</span>
+                  <span class="display-3">35,000</span>
                 </div>
                 <span class="text-small">Discount Price</span>
               </div>
